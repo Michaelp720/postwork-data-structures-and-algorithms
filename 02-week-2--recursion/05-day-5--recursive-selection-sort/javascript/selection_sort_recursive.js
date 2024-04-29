967b6f139a82ede8a6413edbfdf35010dd812283
MichaelPile <mjpile@gmail.com>
2024-04-29 12:26:59 -0400

    completes selection sort recursive

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,5 +1,37 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  //base case
+  if (arr.length === 1 || arr.length === 0){
+    return arr
+  }
+  
+
+  //recursive case - move min to arr[0] then 
+  //selectionSortRecursive(arr.slice(1))
+  const min = Math.min(...arr)
+  const idx = arr.indexOf(min)
+  arr.splice(idx, 1)
+  let sorted = [min, ...selectionSortRecursive(arr)]
+  
+
+  return sorted
+
+
+
+
+  // function selectionSort(arr) {
+  //   const sorted = [];
+  
+  //   while (arr.length > 0) {
+  //     const min = Math.min(...arr);
+  //     const idx = arr.indexOf(min);
+  
+  //     sorted.push(min);
+  //     arr.splice(idx, 1);
+  //   }
+  
+  //   return sorted;
+  // }
 }
 
 if (require.main === module) {
